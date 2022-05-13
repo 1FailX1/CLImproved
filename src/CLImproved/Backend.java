de57ad1fe634a8718538c69281117e013776c743
Hunor <hunor.zakarias@gmail.com>
2022-05-13 17:06:20 +0200

    Added json library

--- src/CLImproved/Backend.java
+++ src/CLImproved/Backend.java
@@ -1,4 +1,13 @@
 package CLImproved;
+import org.json.*;
 
-public class Backend {
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public  class Backend {
+    public static String[] init() {
+    return new String[]{};
+    }
 }
