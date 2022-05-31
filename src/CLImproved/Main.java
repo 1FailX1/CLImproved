a70217fe4ff9aefc5d9e243b2d693acf371af09a
1FailX1 <86916358+1FailX1@users.noreply.github.com>
2022-05-31 18:55:43 +0200

    Main class 0.4 - Added sub-commands + substantially restructured code

--- src/CLImproved/Main.java
+++ src/CLImproved/Main.java
@@ -21,16 +21,18 @@ import static java.lang.Integer.MAX_VALUE;
 
 /**
  * @author 1FailX1 (Felix Payer)
- * @version 0.3
+ * @version 0.4
  */
 
 public class Main extends Application {
-    String[][] commands = {{"d"}, {"d"}};
-    String[][] descriptions = {{"d"}, {"d"}};
+    String[] loaded_commands = {"d"};
+    String[] loaded_descriptions = {"d"};
     ScrollPane scrollPane1 = new ScrollPane();
-    BorderPane borderPane = new BorderPane();
-    VBox vBox1 = new VBox();
-    GridPane gridPane = new GridPane();
+    BorderPane scene_borderPane = new BorderPane();
+    private HBox header_hBox_execmodes;
+    private ScrollPane center_scrollPane;
+    private GridPane center_gridPane;
+    private VBox header_vBox;
 
     /**
      * @param args Main-method for launching the window
@@ -91,8 +93,8 @@ public class Main extends Application {
 
         //-----
         String[] modes = JSONFileHandler.getModes();
-        commands = new String[][]{JSONFileHandler.getWords()}; //Using inner classes requires a final / effectively final one element array (like this one)
-        descriptions = new String[][]{JSONFileHandler.getDescriptions()};
+        loaded_commands = JSONFileHandler.getWords();
+        loaded_descriptions = JSONFileHandler.getDescriptions();
 
         stage.setTitle("CLImproved");
         stage.setResizable(false);
@@ -100,83 +102,87 @@ public class Main extends Application {
         stage.setWidth(1280);
 
         //Main Layout
-        Scene scene = new Scene(borderPane, 1280, 820);
+        Scene scene = new Scene(scene_borderPane, 1280, 820);
         stage.setScene(scene);
 
         //HEADER
-        ImageView imageView1 = new ImageView();
+        ImageView header_logo = new ImageView();
         try {
-            imageView1 = new ImageView(new Image(new FileInputStream("assets\\CLImproved_Logo.png")));
+            header_logo = new ImageView(new Image(new FileInputStream("assets\\CLImproved_Logo.png")));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        HBox hBox1 = new HBox();
-        hBox1.setPrefHeight(100);
-        hBox1.setPrefWidth(MAX_VALUE);
-        hBox1.setAlignment(Pos.CENTER);
-        hBox1.getChildren().add(imageView1);
+        header_hBox_execmodes = new HBox();
+        header_hBox_execmodes.setPrefHeight(100);
+        header_hBox_execmodes.setPrefWidth(MAX_VALUE);
+        header_hBox_execmodes.setAlignment(Pos.CENTER);
+        header_hBox_execmodes.getChildren().add(header_logo);
 
         //Looping through all available modes to create a menu in the header
         for (int i = 0; i < modes.length; i++) {
             Button modeButton = new Button(modes[i]);
             int finalI_mode = i;
             modeButton.setOnAction(actionEvent -> {
+                //Loading the new commands if the mode-changing button is pressed
                 JSONFileHandler.changeMode(finalI_mode);
-                scrollPane1 = new ScrollPane();
-                final VBox[] vBox1 = {new VBox()};
-                final GridPane[] gridPane = {new GridPane()};
-                commands[0] = JSONFileHandler.getWords();
-                descriptions[0] = JSONFileHandler.getDescriptions();
-                gridPane[0].setHgap(50);
+                center_scrollPane = new ScrollPane();
+                header_vBox = new VBox();
+                center_gridPane.getChildren().clear();
+                loaded_commands = JSONFileHandler.getWords();
+                loaded_descriptions = JSONFileHandler.getDescriptions();
+                center_gridPane.setHgap(50);
 
 
-                for (int i1 = 0; i1 < commands[0].length; i1++) {
-                    int finalI_commands = i1;
+                for (int i1 = 0; i1 < loaded_commands.length; i1++) {
+                    int finalI = i1;
                     Button button1 = new Button("Add");
-                    /*button1.setOnAction(actionEvent2 -> {
-                        System.out.println("Button egdrückt");
+                    button1.setOnAction(actionEvent2 -> {
+                        System.out.println("Button gedrückt");
 
-                        if(JSONFileHandler.isParam(finalI)){
+                        if (JSONFileHandler.isParam(finalI)) {
                             System.out.println("Is parameter");
                         }
 
-                        scrollPane1 = new ScrollPane();
-                        vBox1[0] = new VBox();
-                        gridPane[0] = new GridPane();
-                        JSONFileHandler.loadNextWords(finalI_commands);
-                        commands[0] = JSONFileHandler.getWords();
-                        descriptions[0] = JSONFileHandler.getDescriptions();
+                        center_scrollPane = new ScrollPane();
+                        header_vBox = new VBox();
+                        center_gridPane = new GridPane();
+                        JSONFileHandler.loadNextWords(finalI);
+                        loaded_commands = JSONFileHandler.getWords();
+                        loaded_descriptions = JSONFileHandler.getDescriptions();
                         printCurrentCommands();
-                        scrollPane1.setContent(gridPane[0]);
-                        borderPane.setCenter(scrollPane1);
+                        center_scrollPane.setContent(center_gridPane);
+                        scene_borderPane.setCenter(center_scrollPane);
 
                     });
-            */
-                    Label label1 = new Label(commands[0][i1]);
+
+
+                    Label label1 = new Label(loaded_commands[i1]);
                     button1.setPrefWidth(40);
                     label1.setPrefWidth(120);
-                    gridPane[0].add(button1, 0, i1);
-                    gridPane[0].add(label1, 1, i1);
-                    gridPane[0].add(new Label(descriptions[0][i1]), 2, i1);
+                    center_gridPane.add(label1, 1, i1);
+                    center_gridPane.add(button1, 0, i1);
+                    center_gridPane.add(new Label(loaded_descriptions[i1]), 2, i1);
 
                 }
-                vBox1[0].getChildren().add(gridPane[0]);
-                scrollPane1.setContent(vBox1[0]);
-                borderPane.setCenter(scrollPane1);
+                header_vBox.getChildren().add(center_gridPane);
+                scrollPane1.setContent(header_vBox);
+                scene_borderPane.setCenter(scrollPane1);
+
             });
 
 
-            hBox1.getChildren().add(modeButton);
+            header_hBox_execmodes.getChildren().add(modeButton);
         }
 
+
         //CENTER
-        ScrollPane scrollPane1 = new ScrollPane();
-        GridPane gridPane = new GridPane();
-        commands[0] = JSONFileHandler.getWords();
-        descriptions[0] = JSONFileHandler.getDescriptions();
-        gridPane.setHgap(50);
-        for (int i1 = 0; i1 < commands[0].length; i1++) {
+        center_scrollPane = new ScrollPane();
+        center_gridPane = new GridPane();
+        loaded_commands = JSONFileHandler.getWords();
+        loaded_descriptions = JSONFileHandler.getDescriptions();
+        center_gridPane.setHgap(50);
+        for (int i1 = 0; i1 < loaded_commands.length; i1++) {
             int finalI_commands = i1;
             Button button1 = new Button("Add");
             /*button1.setOnAction(actionEvent2 -> {
@@ -191,31 +197,31 @@ public class Main extends Application {
                 commands[0] = JSONFileHandler.getWords();
                 descriptions[0] = JSONFileHandler.getDescriptions();
                 printCurrentCommands();
-                scrollPane1.setContent(gridPane);
-                borderPane.setCenter(scrollPane1);
+                center_scrollPane.setContent(center_gridPane);
+                borderPane.setCenter(center_scrollPane);
 
             }); */
-            Label label1 = new Label(commands[0][i1]);
+            Label label1 = new Label(loaded_commands[i1]);
             button1.setPrefWidth(40);
             label1.setPrefWidth(120);
-            gridPane.add(button1, 0, i1);
-            gridPane.add(label1, 1, i1);
-            gridPane.add(new Label(descriptions[0][i1]), 2, i1);
+            center_gridPane.add(button1, 0, i1);
+            center_gridPane.add(label1, 1, i1);
+            center_gridPane.add(new Label(loaded_descriptions[i1]), 2, i1);
 
         }
 
-        scrollPane1.setContent(gridPane);
+        center_scrollPane.setContent(center_gridPane);
 
         //Adding to Layouts
-        borderPane.setTop(hBox1);
-        borderPane.setCenter(scrollPane1);
-        stage.getIcons().add(imageView1.getImage());
+        scene_borderPane.setTop(header_hBox_execmodes);
+        scene_borderPane.setCenter(center_scrollPane);
+        stage.getIcons().add(header_logo.getImage());
         stage.show();
     }
 
     //Currently not used
     public void printCurrentCommands() {
-        for (int i1 = 0; i1 < commands[0].length; i1++) {
+        for (int i1 = 0; i1 < loaded_commands.length; i1++) {
             int finalI_commands = i1;
             Button button1 = new Button("Add");
             button1.setOnAction(actionEvent2 -> {
@@ -226,22 +232,21 @@ public class Main extends Application {
                         }
                         */
                 scrollPane1 = new ScrollPane();
-                vBox1 = new VBox();
-                gridPane = new GridPane();
+                center_gridPane = new GridPane();
                 JSONFileHandler.loadNextWords(finalI_commands);
-                commands[0] = JSONFileHandler.getWords();
-                descriptions[0] = JSONFileHandler.getDescriptions();
+                loaded_commands = JSONFileHandler.getWords();
+                loaded_descriptions = JSONFileHandler.getDescriptions();
                 printCurrentCommands();
-                scrollPane1.setContent(gridPane);
-                borderPane.setCenter(scrollPane1);
+                scrollPane1.setContent(center_gridPane);
+                scene_borderPane.setCenter(scrollPane1);
 
             });
-            Label label1 = new Label(commands[0][i1]);
+            Label label1 = new Label(loaded_commands[i1]);
             button1.setPrefWidth(40);
             label1.setPrefWidth(120);
-            gridPane.add(button1, 0, i1);
-            gridPane.add(label1, 1, i1);
-            gridPane.add(new Label(descriptions[0][i1]), 2, i1);
+            center_gridPane.add(button1, 0, i1);
+            center_gridPane.add(label1, 1, i1);
+            center_gridPane.add(new Label(loaded_descriptions[i1]), 2, i1);
 
         }
     }
