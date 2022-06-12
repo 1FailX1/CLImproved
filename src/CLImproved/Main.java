ae7d888b68cd942547e142c2973804c5946f3dbf
1FailX1 <86916358+1failx1@users.noreply.github.com>
2022-06-12 21:51:31 +0200

    Main class 0.7 - - refactored code - added save-functionality (in addition to saveAs) - added 2 symbols (save, appearance)

--- src/CLImproved/Main.java
+++ src/CLImproved/Main.java
@@ -1,7 +1,6 @@
 package CLImproved;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
@@ -11,12 +10,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -25,13 +22,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static java.lang.Integer.MAX_VALUE;
 
 /**
  * @author 1FailX1 (Felix Payer)
- * @version 0.6
+ * @version 0.7
  */
 
 public class Main extends Application {
@@ -41,7 +39,7 @@ public class Main extends Application {
     String[] loaded_commands = {"d"};
     String[] loaded_descriptions = {"d"};
     BorderPane scene_borderPane = new BorderPane();
-    private VBox header_vBox_container;
+    Image header_saveSymbol_image;
     private HBox header_hBox_execmodes;
     private ScrollPane center_scrollPane;
     private GridPane center_gridPane;
@@ -49,6 +47,10 @@ public class Main extends Application {
     ImageView header_logo;
     Image header_saveAsSymbol_image;
     ImageView header_saveAsSymbol;
+    ImageView header_saveSymbol;
+    Image header_appearanceSymbol_image;
+    ImageView header_appearanceSymbol;
+    private VBox header_vBox_container = new VBox();
     Image center_addButton_image;
     ImageView center_addButton;
     private TextArea right_textField;
@@ -105,7 +107,6 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
-
         JSONFileHandler.init("prototyp2.json");
         //-----
         String[] modes = JSONFileHandler.getModes();
@@ -126,8 +127,12 @@ public class Main extends Application {
         try {
             header_logo_image = new Image(new FileInputStream("assets\\CLImproved_Logo.png"));
             header_logo = new ImageView(header_logo_image);
-            header_saveAsSymbol_image = new Image(new FileInputStream("assets\\save_icon.png"));
-            header_saveAsSymbol = new ImageView(new Image(new FileInputStream("assets\\save_icon.png")));
+            header_saveAsSymbol_image = new Image(new FileInputStream("assets\\saveAs_icon.png"));
+            header_saveAsSymbol = new ImageView(new Image(new FileInputStream("assets\\saveAs_icon.png")));
+            header_saveSymbol_image = new Image(new FileInputStream("assets\\save_icon.png"));
+            header_saveSymbol = new ImageView(new Image(new FileInputStream("assets\\save_icon.png")));
+            header_appearanceSymbol_image = new Image(new FileInputStream("assets\\appearance_icon.png"));
+            header_appearanceSymbol = new ImageView(new Image(new FileInputStream("assets\\appearance_icon.png")));
             center_addButton_image = new Image(new FileInputStream("assets\\add_button.png"));
             center_addButton = new ImageView(new Image(new FileInputStream("assets\\add_button.png")));
         } catch (Exception e) {
@@ -135,12 +140,12 @@ public class Main extends Application {
         }
         header_saveAsSymbol.setFitWidth(15);
         header_saveAsSymbol.setFitHeight(15);
+        header_saveSymbol.setFitWidth(15);
+        header_saveSymbol.setFitHeight(15);
+        header_appearanceSymbol.setFitWidth(15);
+        header_appearanceSymbol.setFitHeight(15);
 
         //HEADER
-
-
-        header_vBox_container = new VBox();     //Container for the entire header
-
         MenuBar header_menuBar = new MenuBar();
         Menu header_menu1 = new Menu("File");
         Menu header_menu2 = new Menu("Options");
@@ -148,35 +153,30 @@ public class Main extends Application {
         Menu header_menu4 = new Menu("Help");
 
         MenuItem[] header_menu1Items = new MenuItem[2];
-        header_menu1Items[0]= new MenuItem("Save as");
+        header_menu1Items[0] = new MenuItem("Save as");
         header_menu1Items[1] = new MenuItem("Save");
+
+        MenuItem[] header_menu2Items = new MenuItem[1];
+        header_menu2Items[0] = new MenuItem("Appearance");
+
         MenuItem header_menu4Item1 = new MenuItem("About");
-
+        //Functionality for all items below header_menu1
         header_menu1Items[0].setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Save");
-            fileChooser.setInitialFileName("Script_File");
-
-            //Set to user directory or go to default if cannot access
-            String userDirectoryString = System.getProperty("user.home") + "/Desktop";
-            File userDirectory = new File(userDirectoryString);
-            if (!userDirectory.canRead()) {
-                userDirectory = new File("c:/");
-            }
-            fileChooser.setInitialDirectory(userDirectory);
-
-            //Opening a dialog box
-            fileChooser.getExtensionFilters()
-                    .add(new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt"));
-            try {
-                File selectedFile = fileChooser.showSaveDialog(stage);
-                filePathAtLastSave = selectedFile.getAbsolutePath();
-                BufferedWriter output = Files.newBufferedWriter(selectedFile.toPath(), StandardCharsets.UTF_8);
-                contentAtLastSave = CommandWriter.content;
-                output.write(CommandWriter.content);
-                output.flush();
-            } catch (Exception e) {
-                System.out.println("Filesave aborted!");
+            safeAs(stage);
+        });
+        header_menu1Items[1].setOnAction(event -> {
+            System.out.println(contentAtLastSave);
+            if (contentAtLastSave != "") {
+                try {
+                    BufferedWriter output = Files.newBufferedWriter(Paths.get(filePathAtLastSave), StandardCharsets.UTF_8);
+                    contentAtLastSave = CommandWriter.content;
+                    output.write(CommandWriter.content);
+                    output.flush();
+                } catch (Exception e) {
+                    System.out.println("Filesave aborted!");
+                }
+            } else {
+                safeAs(stage);
             }
         });
 
@@ -187,9 +187,16 @@ public class Main extends Application {
             aboutStage.setHeight(700);
             aboutStage.show();
         });
+
         header_menu1Items[0].setGraphic(header_saveAsSymbol);
+        header_menu1Items[1].setGraphic(header_saveSymbol);
         header_menu1.getItems().addAll(header_menu1Items);
+
+        header_menu2Items[0].setGraphic(header_appearanceSymbol);
+        header_menu2.getItems().addAll(header_menu2Items);
+
         header_menu4.getItems().add(header_menu4Item1);
+
         header_menuBar.getMenus().addAll(header_menu1, header_menu2, header_menu3, header_menu4);
 
         header_hBox_execmodes = new HBox();
@@ -211,7 +218,7 @@ public class Main extends Application {
             modeButton.setOnAction(actionEvent -> {
                 //Loading the new commands if the mode-changing button is pressed
                 JSONFileHandler.changeMode(finalI_mode);
-                center_scrollPane = new ScrollPane();
+
                 center_gridPane.getChildren().clear();
                 loaded_commands = JSONFileHandler.getWords();
                 loaded_descriptions = JSONFileHandler.getDescriptions();
@@ -234,8 +241,6 @@ public class Main extends Application {
         loaded_descriptions = JSONFileHandler.getDescriptions();
         center_gridPane.setHgap(50);
         printCurrentCommands();
-        center_scrollPane.setFocusTraversable(false);
-        center_gridPane.setFocusTraversable(false);
         center_scrollPane.setContent(center_gridPane);
 
         //RIGHT
@@ -259,8 +264,32 @@ public class Main extends Application {
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent e) {
-                Platform.exit();
-                System.exit(0);
+                Label secondLabel = new Label("I'm a Label on new Window");
+
+                StackPane secondaryLayout = new StackPane();
+                secondaryLayout.getChildren().add(secondLabel);
+
+                Scene secondScene = new Scene(secondaryLayout, 230, 100);
+
+                // New window (Stage)
+                Stage newWindow = new Stage();
+                newWindow.setTitle("Second Stage");
+                newWindow.setScene(secondScene);
+
+                // Specifies the modality for new window.
+                newWindow.initModality(Modality.WINDOW_MODAL);
+
+                // Specifies the owner Window (parent) for new window
+                newWindow.initOwner(stage);
+
+                // Set position of second window, related to primary window.
+                newWindow.setX(stage.getX() + 200);
+                newWindow.setY(stage.getY() + 100);
+
+                newWindow.show();
+                //killing the process
+                //Platform.exit();
+                //System.exit(0);
             }
         });
     }
@@ -294,6 +323,8 @@ public class Main extends Application {
                 scene_borderPane.setCenter(center_scrollPane);
                 //Refreshing the TextField
                 right_textField.setText(CommandWriter.content);
+                right_textField.setScrollTop(Double.MAX_VALUE);
+                right_textField.positionCaret(CommandWriter.content.length());
             });
 
             Label label1 = new Label(loaded_commands[i1]);
@@ -305,6 +336,78 @@ public class Main extends Application {
 
         }
 
+    }
+    /*public void printCurrentCommands() {
+        center_gridPane.setHgap(50);
+        center_gridPane.setVgap(5);
+
+        // System.out.println(Arrays.toString(loaded_commands));
+        for (int i1 = 0; i1 < loaded_commands.length; i1++) {
+            int finalI_commands = i1;
+            Button button1 = new Button();
+            button1.setBackground(null);
+            button1.setGraphic(new ImageView(center_addButton_image));
+            button1.setFocusTraversable(false);
+            button1.setOnAction(actionEvent2 -> {
+                //    System.out.println("Button egdrückt")
+                if (JSONFileHandler.isParam(finalI_commands)) {
+                    String parameter = PopUp.readLine(JSONFileHandler.getWords()[finalI_commands]);
+                    System.out.println(parameter);
+                    CommandWriter.writeWord(parameter);
+                    System.out.println("Is parameter");
+                }
+
+                center_gridPane.getChildren().clear();
+                JSONFileHandler.loadNextWords(finalI_commands);
+                loaded_commands = JSONFileHandler.getWords();
+                loaded_descriptions = JSONFileHandler.getDescriptions();
+                printCurrentCommands();
+                center_scrollPane.setContent(center_gridPane);
+                scene_borderPane.setCenter(center_scrollPane);
+                //Refreshing the TextField
+                right_textField.setText(CommandWriter.content);
+                right_textField.setScrollTop(Double.MAX_VALUE);
+                right_textField.positionCaret(CommandWriter.content.length());
+            });
+
+            Label label1 = new Label(loaded_commands[i1]);
+            button1.setPrefWidth(40);
+            label1.setPrefWidth(120);
+            center_gridPane.add(button1, 0, i1);
+            center_gridPane.add(label1, 1, i1);
+            center_gridPane.add(new Label(loaded_descriptions[i1]), 2, i1);
+
+        }
+
+    }
+    */
+
+    private void safeAs(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        fileChooser.setInitialFileName("Script_File");
+
+        //Set to user directory or go to default if cannot access
+        String userDirectoryString = System.getProperty("user.home") + "/Desktop";
+        File userDirectory = new File(userDirectoryString);
+        if (!userDirectory.canRead()) {
+            userDirectory = new File("c:/");
+        }
+        fileChooser.setInitialDirectory(userDirectory);
+
+        //Opening a dialog box
+        fileChooser.getExtensionFilters()
+                .add(new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt"));
+        try {
+            File selectedFile = fileChooser.showSaveDialog(stage);
+            filePathAtLastSave = selectedFile.getAbsolutePath();
+            BufferedWriter output = Files.newBufferedWriter(selectedFile.toPath(), StandardCharsets.UTF_8);
+            contentAtLastSave = CommandWriter.content;
+            output.write(CommandWriter.content);
+            output.flush();
+        } catch (Exception e) {
+            System.out.println("Filesave aborted!");
+        }
     }
 
 }
