7160872c6750c24d872a1aec6d7bd5a1eeb63c99
Hunor <hunor.zakarias@gmail.com>
2022-06-13 23:00:54 +0200

    heavily extended json file created new ciscoFile.json which will be the final file documented CommandWriter.java class with javadoc

--- src/CLImproved/CommandWriter.java
+++ src/CLImproved/CommandWriter.java
@@ -5,34 +5,44 @@ public class CommandWriter {
     static String tabs = "";
     static private boolean lineJumpMade = true;
 
-
-    public static void writeLine(String s) {
-        content += tabs + s + "\n";
-    }
-
+    /**
+     * @param s writes a String to the last line without making a break
+     */
     public static void writeWord(String s) {
         if (lineJumpMade) {
             lineJumpMade = false;
             content += tabs;
         } else {
-            content+=" ";
+            content += " ";
         }
         content += s;
     }
 
+    /**
+     * makes a break
+     */
     public static void makeBreak() {
         content += "\n";
         lineJumpMade = true;
     }
 
+    /**
+     * adds a tab which then are added after evey break
+     */
     public static void addTab() {
         tabs += "\t";
     }
 
+    /**
+     * removes a tab which then are added after evey break
+     */
     public static void removeTab() {
         tabs = tabs.substring(1);
     }
 
+    /**
+     * makes a break
+     */
     public static String getString() {
         return content;
     }
